4a605902ddf384d76f9a3b010256cc67f6fc6680
Zack Moore <zmoore@usgs.gov>
2019-02-25 15:36:15 -0600

    Use a single instance of ObjectMapper, based on feedback from Carl.

--- src/main/java/gov/usgs/wma/mlrnotification/config/OAuth2ResourceServerConfig.java
+++ src/main/java/gov/usgs/wma/mlrnotification/config/OAuth2ResourceServerConfig.java
@@ -30,6 +30,8 @@ public class OAuth2ResourceServerConfig extends ResourceServerConfigurerAdapter 
 	@Value("${security.oauth2.resource.id}")
 	private String resourceId;
 
+	private final ObjectMapper mapper = new ObjectMapper();
+
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		http
@@ -62,11 +64,8 @@ public class OAuth2ResourceServerConfig extends ResourceServerConfigurerAdapter 
 						final Map<String, Object> mapBodyException = new HashMap<>() ;
 
 						mapBodyException.put("error_message", authException.getMessage()) ;
-				
 						response.setContentType("application/json") ;
 						response.setStatus(HttpServletResponse.SC_UNAUTHORIZED) ;
-				
-						final ObjectMapper mapper = new ObjectMapper() ;
 						mapper.writeValue(response.getOutputStream(), mapBodyException) ;
 			}
 		};
